69974fcae3860271c811a414c092db45c23f648a
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2021-04-03 23:46:00 -0700

    update resumedata

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,96 +1,89 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Stephanie Cherubin",
-    "role": "Frontend Developer & Full Stack",
-    "linkedinId":"Your LinkedIn Id",
-    "twitterid": "Your twitterid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or make Javascript games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/stephanie-cherubin/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/StephanieCherubin/",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"twitter",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a senior year student at Make School and pursuing my B.S. I am a Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"...",
-    "website":"",
-    "education":[
+  "imagebaseurl":"https://rbhatia46.github.io/",
+  "name": "Stephanie Cherubin",
+  "role": "Frontend & Full Stack Developer",
+  "linkedinId":"Your LinkedIn Id",
+  "twitterid": "Your twitterid",
+  "roleDescription": " I like dabbling in various parts of frontend development like libraries like React and like to learn about new technologies, write technical articles or volunteer",
+  "socialLinks":[
       {
-        "UniversityName":"Make School",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/stephanie-cherubin/",
+        "className":"fa fa-linkedin"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "name":"github",
+        "url":"https://github.com/StephanieCherubin/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"twitter",
+        "url":"http://twitter.com/savvy_n_sweet",
+        "className":"fa fa-twitter"
       }
     ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-  }
-  
-  export default resumeData
+  "aboutme":"I am currently a student at Make School pursuing my Bachelor's Degree in Science. I am a Full Stack Web Developer. Results-oriented, driven professional with a proven track record of demonstrating excellence. I am a lifelong learner currently taking classes to further my professional development. #NeverStopLearning Check out my Plum results here: app.plum.io/profile/80ff2cf4d5f9bb72bcdf741341655ffeb6c81b6b     ",
+  "address":"San Francisco, CA",
+  "website":"https://www.makeschool.com/portfolio/Stephanie-Cherubin",
+  "education":[
+    {
+      "UniversityName":"Make School",
+      "specialization":"Front end Development",
+      "MonthOfPassing":"Aug",
+      "YearOfPassing":"2020",
+      "Achievements":"Some Achievements"
+    },
+  ],
+  "work":[
+    {
+      "CompanyName":"Black Girls Code",
+      "specialization":"Teaching computer programming",
+      "MonthOfLeaving":"August",
+      "YearOfLeaving":"2019",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Octopi",
+      "specialization":"Software Engineering",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",  
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+}
+
+export default resumeData
